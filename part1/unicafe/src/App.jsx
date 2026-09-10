0c6785e6d467441806504ad1465f9536c7a91867
Matej Milankov <matej.milankov@gmail.com>
2026-09-11 00:13:03 +0200

    Unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad;
+  const avg = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * (-1)) / total;
+  const positive = total === 0 ? `0%` : `${good / total * 100}%`;
+
   const handleGoodClick = () => {
     setGood(prev => prev + 1);
   }
@@ -31,6 +35,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {avg}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
