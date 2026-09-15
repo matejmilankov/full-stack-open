7f44b9012326656b070ab3dcc5c8482987ae1192
Matej Milankov <matej.milankov@gmail.com>
2026-09-15 17:57:10 +0200

    feat: add info endpoint

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+    const currentDate = new Date().toString();
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${currentDate}</p>
+    `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
