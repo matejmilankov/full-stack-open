175368df2c5168cb3f9315ba2f93f425ed3adc1b
Matej Milankov <matej.milankov@gmail.com>
2026-09-15 18:23:54 +0200

    feat: add endpoint for creating person

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
     { 
@@ -23,6 +24,10 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const genereateId = () => {
+    return String(Math.floor(Math.random() * 1000000 + 1));
+}
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
@@ -49,7 +54,38 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id;
     persons = persons.filter(p => p.id !== id);
     response.status(204).end();
-})
+});
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        });
+    }
+    
+    if(!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        });
+    }
+
+    if(persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const person = {
+        id: genereateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person);
+
+    return response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
