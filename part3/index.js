8ed06b7fee420828f5494dddeff6aa176603e252
Matej Milankov <matej.milankov@gmail.com>
2026-09-16 16:26:07 +0200

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(express.static('dist'));
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body);
@@ -97,7 +100,7 @@ app.post('/api/persons', (request, response) => {
     return response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
