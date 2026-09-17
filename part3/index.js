cda6c4b74018df6e9c544f2adba19cf3d86e602c
Matej Milankov <matej.milankov@gmail.com>
2026-09-17 14:58:22 +0200

    refactor: extract mongodb configuration to module and fetch persons from atlas

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
+const Phonebook = require('./models/phonebook');
 
 const app = express();
 
+// Middleware
 app.use(express.static('dist'));
 app.use(express.json());
 
@@ -13,35 +16,15 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
+// Endpoints
 const genereateId = () => {
     return String(Math.floor(Math.random() * 1000000 + 1));
 }
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Phonebook.find({}).then(persons => {
+        response.json(persons);
+    });
 });
 
 app.get('/info', (request, response) => {
@@ -53,12 +36,9 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.find(p => p.id === id);
-    if(person)
-        response.json(person);
-    else
-        response.status(404).end();
+    Phonebook.findById(request.params.id).then(phonebook => {
+        response.json(phonebook);
+    });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -98,7 +78,7 @@ app.post('/api/persons', (request, response) => {
     return response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
