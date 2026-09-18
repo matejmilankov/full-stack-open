c9ce1e07bea61b4eaac2d328259c5937ec7d38da
Matej Milankov <matej.milankov@gmail.com>
2026-09-19 01:05:50 +0200

    test: add favoriteBlog helper and unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,21 @@
 const dummy = (blogs) => {
-  return 1;
+    return 1;
 }
 
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0
+        ? null
+        : blogs.reduce((fav, blog) => {
+            return blog.likes > fav.likes ? blog : fav;
+        }, blogs[0]);
+}
+
 module.exports = {
-  dummy,
-  totalLikes
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
