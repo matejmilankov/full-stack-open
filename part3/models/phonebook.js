3066602cbf5b461c13c7c69de63cc4e9722833d5
Matej Milankov <matej.milankov@gmail.com>
2026-09-18 17:32:12 +0200

    feat: add custom validation on db side

--- part3/models/phonebook.js
+++ part3/models/phonebook.js
@@ -9,14 +9,24 @@ mongoose.connect(url, { family: 4 })
     })
     .catch(error => {
         console.log('error connecting to MongoDB', error.message);
-    })
+    });
+
 
 const phonebookSchema = mongoose.Schema({
     name: {
         type: String,
         minLength: 3
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!\n`
+        }
+    }
 });
 phonebookSchema.set('toJSON', {
     transform: (document, returnedObject) => {
