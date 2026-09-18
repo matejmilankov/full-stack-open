56be21c246041e6525057ef5e0a6c94d934a2248
Matej Milankov <matej.milankov@gmail.com>
2026-09-18 20:21:18 +0200

    refactor: refactore middleware code into utils folder

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
-const blogRouter = require('./controllers/blogs')
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const blogRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 
 const app = express()
 
+mongoose.connect(config.MONGODB_URI, { family: 4 });
 
+app.use(express.json());
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
+app.use('/api/blogs', blogRouter);
 
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-
-
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 
 app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
-})
+    logger.info(`Server running on port ${config.PORT}`);
+});
