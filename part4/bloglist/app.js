786b1dd92387752caa88f9e0cc53ae19223aa604
Matej Milankov <matej.milankov@gmail.com>
2026-09-18 20:33:55 +0200

    refactor: refactor index.js so now it only runs server

---
+++ part4/bloglist/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const blogRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { family: 4 });
+
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,21 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const app = require('./app');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
-const blogRouter = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
-
-const app = express()
-
-mongoose.connect(config.MONGODB_URI, { family: 4 });
-
-app.use(express.json());
-
-app.use('/api/blogs', blogRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
