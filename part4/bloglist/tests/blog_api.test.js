ea3805d616541c38c14675cbaadf7ad34f419634
Matej Milankov <matej.milankov@gmail.com>
2026-09-19 22:27:02 +0200

    refactor(bloglist): refacot blog_api into semantically meaningful units

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test');
+const { test, after, beforeEach, describe } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
@@ -9,100 +9,108 @@ const helper = require('./test_helper');
 // superagent object
 const api = supertest(app);
 
-beforeEach(async () => {
-    await Blog.deleteMany({});
-    await Blog.insertMany(helper.initialBlogs);
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({});
+        await Blog.insertMany(helper.initialBlogs);
+    });
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    });
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs');
+        assert.strictEqual(response.body.length, helper.initialBlogs.length);
+    });
+    test('unique identifier property of the blog posts is named id', async () => {
+        const response = await api.get('/api/blogs');
+        assert(response.body[0].id);
+    });
+
+    
+    describe('addition of a new blog', () => {
+        test('succeeds with valid data', async () => {
+            const newBlog = {
+                title: 'async await is cool',
+                author: 'Dan Abramov',
+                url: 'https://react.dev',
+                likes: 3
+            }
+        
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+        
+            const blogsAtEnd = await helper.blogsInDb();
+            const titles = blogsAtEnd.map(blog => blog.title);
+        
+            assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+            assert(titles.includes('async await is cool'));
+        });
+        test('defaults to 0 likes if likes property is missing', async () => {
+            const newBlog = {
+                title: 'fullstack is fun',
+                author: 'Dan Abramov',
+                url: 'https://react.dev',
+            }
+        
+            const response = await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+        
+            assert.strictEqual(response.body.likes, 0);
+        });
+        
+        test('fails with status code 400 if url is missing', async () => {
+            const newBlog = {
+                title: 'http is powerfull',
+                author: 'Dan Abramov',
+                likes: 3
+            }
+        
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
+        
+            const blogs = await helper.blogsInDb();
+            assert.strictEqual(blogs.length, helper.initialBlogs.length);
+        });
+        
+        test('fails with status code 400 if title is missing', async () => {
+            const newBlog = {
+                author: 'Dan Abramov',
+                url: 'https://react.dev',
+                likes: 3
+            }
+        
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
+        
+            const blogs = await helper.blogsInDb();
+            assert.strictEqual(blogs.length, helper.initialBlogs.length);
+        });
+    });
+
+
+    after(async () => {
+        await mongoose.connection.close();
+    });
 });
 
 
 
-test('blogs are returend as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-
-after(async () => {
-    await mongoose.connection.close();
-})
-
-test('all blogs are returend', async () => {
-    const response = await api.get('/api/blogs');
-    assert.strictEqual(response.body.length, helper.initialBlogs.length);
-});
 
 
-test('unique identifier property of the blog posts is named id', async () => {
-    const response = await api.get('/api/blogs');
-    assert(response.body[0].id);
-});
 
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        title: 'async await is cool',
-        author: 'Dan Abramov',
-        url: 'https://react.dev',
-        likes: 3
-    }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb();
-    const titles = blogsAtEnd.map(blog => blog.title);
 
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-    assert(titles.includes('async await is cool'));
-});
-
-test('likes propery is missing', async () => {
-    const newBlog = {
-        title: 'fullstack is fun',
-        author: 'Dan Abramov',
-        url: 'https://react.dev',
-    }
-
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    assert.strictEqual(response.body.likes, 0);
-});
-
-test('blog without url is not added', async () => {
-    const newBlog = {
-        title: 'http is powerfull',
-        author: 'Dan Abramov',
-        likes: 3
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    const blogs = await helper.blogsInDb();
-    assert.strictEqual(blogs.length, helper.initialBlogs.length);
-});
-
-test('blog without title is not added', async () => {
-    const newBlog = {
-        author: 'Dan Abramov',
-        url: 'https://react.dev',
-        likes: 3
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    const blogs = await helper.blogsInDb();
-    assert.strictEqual(blogs.length, helper.initialBlogs.length);
-});
